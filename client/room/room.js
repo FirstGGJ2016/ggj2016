96da4c3507e840816f358931dfc3aebfcee3ae07
Francisco López <donflopez@gmail.com>
2016-01-29 21:45:05 +0100

    Add first room logic

--- client/room/room.js
+++ client/room/room.js
@@ -1,6 +1,50 @@
-Template.room.helper({
+Template.room.helpers({
   room: function() {
+    return Rooms.findOne();
+  },
+});
 
-    return Rooms.findOne({ users:  { $size: 1 } });
+function is(person) {
+  return person ? 'person' : 'spirit';
+}
+
+function createRoom(id, person) {
+  var room = {
+        owner: id,
+      };
+
+  room.person = { user: person };
+  room.spirit = { user: !person };
+
+  room[is(person)].id = id;
+
+  Rooms.insert(room);
+}
+
+function enterInARoom(id, person) {
+  var query = {};
+
+  query[is(!person) + '.user'] = true;
+
+  var room = Rooms
+    .findOne(query);
+
+  if (room) {
+    room[is(person)].user = true;
+    room[is(person)].id = id;
+
+    Rooms.update(room._id, room);
+    return room;
+  } else {
+    createRoom(id, person);
+  }
+}
+
+Template.room.events({
+  'click #start': function() {
+    var user = Meteor.user();
+    if (user) {
+      enterInARoom(Meteor.userId(), user.profile.isPerson);
+    }
   },
 });
