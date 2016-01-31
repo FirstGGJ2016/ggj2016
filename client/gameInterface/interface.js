a40e7dd90f58924ea56f3eee3eaf6225f4d5ec74
Francisco López <donflopez@gmail.com>
2016-01-31 14:21:32 +0100

    Send word logic

--- client/gameInterface/interface.js
+++ client/gameInterface/interface.js
@@ -1,4 +1,35 @@
+var nextWord = true;
+
 Template.gameInterface.created = function() {
   //TODO: call here the .play() method of the game.
   _room.play(Meteor.user().profile.isPerson ? 'person' : 'spirit');
+
+  var input = $('#word');
+
+  Room.onWord(function(word) {
+    _room.spellWord(word, function() {
+      input.value = '';
+    });
+  });
 };
+
+Template.gameInterface.helpers({
+  isSpirit: function() {
+    return !Meteor.user().profile.isPerson;
+  },
+});
+
+Template.gameInterface.events({
+  'keypress #word': function(e) {
+    if (e.keyCode === 13 && nextWord) {
+      var input = $('#word')[0];
+      nextWord = false;
+      Room.emitWord(input.value);
+
+      _room.spellWord(input.value, function() {
+        input.value = '';
+        nextWord = true;
+      });
+    }
+  },
+});
--- client/lib/rtc/room_handling.js
+++ client/lib/rtc/room_handling.js
@@ -19,3 +19,32 @@ Room.isCaller = function(room) {
 Room.isCompleted = function(id) {
   return Rooms.findOne({ _id:id, 'person.user': true, 'spirit.user': true });
 };
+
+Room.emitMessage = function(id, channel, message) {
+  Streamy.broadcast(id + channel, {
+    user: Meteor.userId(),
+    message: message,
+  });
+};
+
+Room.subscribeChanel = function(id, channel, cb) {
+  Streamy.on(id + channel, function(data) {
+    if (data.user !== Meteor.userId()) {
+      cb(data);
+    }
+  });
+};
+
+Room.emitWord = function(word) {
+  var room = this.getRoom();
+
+  this.emitMessage(room._id, 'word', word);
+};
+
+Room.onWord = function(cb) {
+  var room = this.getRoom();
+
+  this.subscribeChanel(room._id, 'word', function(res) {
+    cb(res.message);
+  });
+};
